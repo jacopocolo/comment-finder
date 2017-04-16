45df35c432b0f5a2e70bf2ca8d6e69202bb299f4
Jacopo Colò <jacopocolo@gmail.com>
2017-04-16 18:11:04 +0200

    New icon and it now works on CSS, JS and HTML

--- js/detect.js
+++ js/detect.js
@@ -2,84 +2,128 @@
 //Match inline HTML, JS, CSS comments
 //Match external CSS comments
 //Match external JS comments
-//Display found comments in icon
+//Display found comments in icon http://stackoverflow.com/questions/5759130/google-chrome-extension-numbers-on-the-icon
 //Create a popup that shows the comments
 
-// javascript: (function() {
-//     (function() {
-//         function loadCSS(filename) {
-//             var file = document.createElement("link");
-//             file.setAttribute("rel", "stylesheet");
-//             file.setAttribute("type", "text/css");
-//             file.setAttribute("href", filename);
-//             if (typeof file !== "undefined");
-//             document.getElementsByTagName("head")[0].appendChild(file);
-//         };
-//         loadCSS("https://rawgit.com/jacopocolo/detector/master/cd.css?x=" + (Math.random()));
-//       var comDet = function() {
-//           var html = document.getElementsByTagName('html')[0].innerHTML;
-//             var matchHtml = html.match(/<!--/g);
-//             if (matchHtml === null) {
-//                 console.log('There are no comments here');
-//             } else {
-//                 for (var x = 0; x < matchHtml.length; x = x + 1) {
-//                     var inizioCommento = html.search('<!--');
-//                     var fineCommento = html.search('-->');
-//                     var commento = html.substring(inizioCommento + 4, fineCommento);
-//                     var html = html.substring(fineCommento + 3);
-//                     var newElement = '<div id="commentdetector"><pre>' + commento + '</pre></div>';
-//                     var bodyElement = document.body;
-//                     bodyElement.innerHTML = newElement + bodyElement.innerHTML;
-//                 }
-//             }
-//         };
-//         comDet();
-//     })()
-// })()
+var totalComments = 0;
 
-
-
-
-function loadXMLDoc(theURL)
-{
-    if (window.XMLHttpRequest)
-    {
-        xmlhttp=new XMLHttpRequest();
+//UTILS
+function loadXMLDoc(myurl, type) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            console.log(xmlhttp.responseText);
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          if (type == "css") {
+            parseCss(xmlhttp.responseText, myurl);
+          }
+          if (type == "js") {
+            parseJs(xmlhttp.responseText, myurl);
+          }
         }
     }
-    xmlhttp.open("GET", theURL, true);
+
+    xmlhttp.open("GET", myurl, true);
     xmlhttp.send();
+    return xmlhttp.onreadystatechange();
 }
 
-var js = document.getElementsByTagName('script');
-for (x=0;x<js.length;x++) {
-  if (js[x].src.includes(".js") &&
-      js[x].src.includes(window.location.hostname) &&
-      !js[x].src.includes("min.js") &&
-      !js[x].src.includes("google")
-    ) {
-      console.log("✅ "+js[x].src);
-      //loadXMLDoc(js[x].src);
+//HTML Comments
+var findHtml = function() {
+    var html = document.getElementsByTagName('html')[0].innerHTML;
+    var matchHtml = html.match(/<!--[\s\S]*?-->/g);
+    if (matchHtml === null) {
+        console.log('No HTML comments');
     } else {
-      console.log("js filtered: "+js[x].src)
+        for (x=0;x<matchHtml.length;x++) {
+            console.log(matchHtml[x])
+        }
+        totalComments+=x;
+    }
+    var matchCss = html.match(/\/\*[^*]*\*+([^/*][^*]*\*+)*\//);
+    if (matchCss === null) {
+        console.log('No CSS comments in HTML');
+    } else {
+        for (x=0;x<matchCss.length;x++) {
+            console.log(matchCss[x])
+        }
+        totalComments+=x;
+    }
+    var matchJs = html.match(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm);
+    if (matchJs === null) {
+        console.log('No js comments in HTML');
+    } else {
+        for (x=0;x<matchJs.length;x++) {
+            console.log(matchJs[x])
+        }
+        totalComments+=x;
+    }
+};
+
+//CSS Comments
+var findCss = function() {
+    var css = document.getElementsByTagName('link');
+    for (x = 0; x < css.length; x++) {
+        if (css[x].rel == "stylesheet" &&
+            css[x].href.includes(window.location.hostname) &&
+            css[x].href.includes(".css") &&
+            !css[x].href.includes("min.css")
+        ) {
+            loadXMLDoc(css[x].href, "css");
+        } else {
+            console.log("css filtered: " + css[x].href)
+        }
     }
 }
 
-var css = document.getElementsByTagName('link');
-for (x=0;x<css.length;x++) {
-  if (css[x].rel == "stylesheet"
-      && css[x].href.includes(".css")
-      && !css[x].href.includes("min.css")
-    ) {
-      console.log("✅ "+css[x].href);
-      //loadXMLDoc(css[x].href);
+var parseCss = function(string, source) {
+  console.log("✅ " + source);
+    cssMatch = string.match(/(\/\*([\s\S]*?)\*\/)|(\/\/(.*)$)/gm);
+    if (cssMatch === null) {
+        console.log('No CSS comments');
     } else {
-      console.log("css filtered:"+css[x].href)
+        for (x=0;x<cssMatch.length;x++) {
+            console.log(cssMatch[x]);
+        }
+        totalComments += x;
+        console.log(totalComments);
     }
 }
+
+//Javascript comments
+var findJs = function() {
+  var js = document.getElementsByTagName('script');
+  for (x = 0; x < js.length; x++) {
+      if (js[x].src.includes(".js")
+          && js[x].src.includes(window.location.hostname)
+          && !js[x].src.includes("min.js")
+          && !js[x].src.includes("google")
+      ) {
+          loadXMLDoc(js[x].src, "js");
+      } else {
+          console.log("js filtered: " + js[x].src)
+      }
+  }
+}
+
+var parseJs = function(string, source) {
+  console.log("✅ " + source);
+    jsMatch = string.match(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm);
+    if (jsMatch === null) {
+        console.log('No JS comments');
+    } else {
+        for (x=0;x<jsMatch.length;x++) {
+            console.log(jsMatch[x]);
+        }
+        totalComments += x;
+        console.log(totalComments);
+    }
+}
+
+findHtml();
+findCss();
+findJs();
