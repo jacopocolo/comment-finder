221cef2b782bf2dbc7789d491814ade4e0c7ca0b
Jacopo Colò <jacopocolo@gmail.com>
2018-01-09 23:00:58 +0100

    Works better than before
    There’s a hotfix in the else at the end of carrots setup when the
    carrots has 0 entries that I don’t really like but I’m not sure how to
    fix it in another way

--- js/background.js
+++ js/background.js
@@ -16,14 +16,19 @@ function resetBadge() {
   });
 }
 
+//Used by the activated, the load-complete reports and send message functions
 var currentTab;
+
+
+//SET THE BADGE PROPERLY FUNCTIONS
 
 //If the page is activated
 //On newtabs, this triggers chrome://newtab/ and NOT the url you are entering
-//So the only solution is to rely on load complete reports
+//So the only solution is to rely on load-complete reports
 chrome.tabs.onActivated.addListener(function(info) {
   resetBadge();
   var tab = chrome.tabs.get(info.tabId, function(tab) {
+    currentTab = tab.url;
     for (x = 0; x < carrots.length; x++) {
       if (carrots[x].url == tab.url) {
         var totalComments = carrots[x].html.length + carrots[x].css.length + carrots[x].js.length;
@@ -38,7 +43,7 @@ chrome.tabs.onActivated.addListener(function(info) {
   });
 });
 
-//Load complete reports
+//Load-complete reports
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     //We ask Chrome what's the current tab
@@ -48,38 +53,33 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     }, function(tabs) {
       currentTab = tabs[0].url;
       tabJustCompleted = tab.url;
+
       if (currentTab == tabJustCompleted) {
         for (x = 0; x < carrots.length; x++) {
+
           if (carrots[x].url == currentTab) {
             var totalComments = carrots[x].html.length + carrots[x].css.length + carrots[x].js.length;
             chrome.browserAction.setBadgeBackgroundColor({
-              color: [0, 0, 0, 255]
+              color: [0, 0, 255, 255]
             });
             chrome.browserAction.setBadgeText({
               text: totalComments.toString()
             });
           }
+
         };
       }
-
     });
   }
 });
+
+//SEND THE COMMENTS TO POPUP
 
 chrome.runtime.onMessage.addListener(
   function(msg, sender, sendResponse) {
     if (msg.html) {
       var totalComments = msg.html.length + msg.css.length + msg.js.length;
       if (totalComments > 0) {
-        
-        //Remove this to see if it's making a mess
-        /* chrome.browserAction.setBadgeBackgroundColor({
-          color: [255, 0, 0, 255]
-        });
-        chrome.browserAction.setBadgeText({
-          text: totalComments.toString()
-        }); */
-
         //push msg in the array making sure we are not storig copies
         //Check if the array has something inside
         if (carrots.length > 0) {
@@ -95,14 +95,21 @@ chrome.runtime.onMessage.addListener(
               return
             }
           }
-          //If the array has 0 entries, add the first one
+          //If the array has 0 entries, add the first one and set the badge
         } else {
           carrots.push(msg)
+          chrome.browserAction.setBadgeBackgroundColor({
+            color: [255, 0, 0, 255]
+          });
+          chrome.browserAction.setBadgeText({
+            text: totalComments.toString()
+          });
         };
       } else {
         resetBadge();
       }
     }
+
     if (msg.method == "getComments") {
       for (x = 0; x < carrots.length; x++) {
         if (carrots[x].url == currentTab) {
